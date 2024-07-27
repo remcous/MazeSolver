126bf040721473cae3a0a0537a66ce8425b9e3d5
remcous <67499364+remcous@users.noreply.github.com>
2024-07-27 14:26:43 -0700

    Draw Lines

--- graphics.py
+++ graphics.py
@@ -1,11 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack()
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,3 +20,20 @@ class Window():
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x=0,  y=0):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800,600)
+    l = Line(Point(0,0), Point(800,600))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
