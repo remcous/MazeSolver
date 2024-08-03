cccaf3d4aad2c042780df151d14eac1146cd932d
remcous <67499364+remcous@users.noreply.github.com>
2024-08-02 17:27:13 -0700

    Break all walls recursively to create the maze and reset the cell visited flag to false

--- maze.py
+++ maze.py
@@ -1,27 +1,34 @@
 from cell import Cell
 from time import sleep
+import random
 
 class Maze():
     def __init__(self,
                  x1,
                  y1, 
-                 num_rows,
                  num_cols,
+                 num_rows,
                  cell_size_x,
                  cell_size_y,
                  win,
+                 seed=None,
                  ):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
-        self._num_rows = num_rows
         self._num_cols = num_cols
+        self._num_rows = num_rows
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
 
+        if seed is not None:
+            random.seed(seed)
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         self._cells = [[Cell(self._win) for j in range(self._num_rows)] for i in range(self._num_cols)]
@@ -50,3 +57,49 @@ class Maze():
         self._draw_cell(0,0)
         self._cells[self._num_cols-1][self._num_rows-1].has_bottom_wall = False
         self._draw_cell(self._num_cols-1, self._num_rows-1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            possible_moves = []
+
+            # find possible moves
+            if i > 0 and not self._cells[i-1][j].visited:
+                possible_moves.append("left")
+                
+            if i < self._num_cols - 1 and not self._cells[i+1][j].visited:
+                possible_moves.append("right")
+
+            if j > 0 and not self._cells[i][j-1].visited:
+                possible_moves.append("up")
+
+            if j < self._num_rows - 1 and not self._cells[i][j+1].visited:
+                possible_moves.append("down")
+
+            if len(possible_moves) == 0:
+                self._draw_cell(i,j)
+                return
+            
+            chosen_move = possible_moves[random.randrange(len(possible_moves))]
+
+            if chosen_move == "left":
+                self._cells[i][j].has_left_wall = False
+                self._cells[i-1][j].has_right_wall = False
+                self._break_walls_r(i-1, j)
+            elif chosen_move == "right":
+                self._cells[i][j].has_right_wall = False
+                self._cells[i+1][j].has_left_wall = False
+                self._break_walls_r(i+1, j)
+            elif chosen_move == "up":
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j-1].has_bottom_wall = False
+                self._break_walls_r(i, j-1)
+            elif chosen_move == "down":
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j+1].has_top_wall = False
+                self._break_walls_r(i, j+1)
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ class Tests(unittest.TestCase):
     def test_maze_create_cells1(self):
         num_cols = 12
         num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        m1 = Maze(0, 0, num_cols, num_rows, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -16,9 +16,9 @@ class Tests(unittest.TestCase):
         )
 
     def test_maze_create_cells2(self):
-        num_cols = 200
-        num_rows = 320
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        num_cols = 20
+        num_rows = 32
+        m1 = Maze(0, 0, num_cols, num_rows, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -31,7 +31,7 @@ class Tests(unittest.TestCase):
     def test_maze_create_cells3(self):
         num_cols = 4
         num_rows = 2
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        m1 = Maze(0, 0, num_cols, num_rows, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -44,16 +44,24 @@ class Tests(unittest.TestCase):
     def test_maze_break_entrance_and_exit1(self):
         num_cols = 1
         num_rows = 1
-        m = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        m = Maze(0, 0, num_cols, num_rows, 10, 10, None)
         self.assertEquals(m._cells[0][0].has_top_wall, False)
         self.assertEquals(m._cells[0][0].has_bottom_wall, False)
 
     def test_maze_break_entrance_and_exit2(self):
         num_cols = 10
         num_rows = 10
-        m = Maze(0, 0, num_rows, num_cols, 10, 10, None)
-        self.assertEquals(m._cells[0][0].has_top_wall, False)
-        self.assertEquals(m._cells[9][9].has_bottom_wall, False)
+        m = Maze(0, 0, num_cols, num_rows, 10, 10, None)
+        self.assertFalse(m._cells[0][0].has_top_wall)
+        self.assertFalse(m._cells[9][9].has_bottom_wall)
+
+    def test_reset_all_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m = Maze(0, 0, num_cols, num_rows, 10, 10, None)
+        for col in m._cells:
+            for cell in col:
+                self.assertFalse(cell.visited)
 
 if __name__ == '__main__':
     unittest.main()
